8b9dea284832f92016326de0f1f061f6ea0a119b
fly_6 <36567300+epfly6@users.noreply.github.com>
2024-11-15 11:53:40 +0300

    feat: Rectangle completed

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -19,8 +19,15 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return Object.create({
+    getArea() {
+      return width * height;
+    },
+  }, {
+    width: { value: width },
+    height: { value: height },
+  });
 }
 
 /**
