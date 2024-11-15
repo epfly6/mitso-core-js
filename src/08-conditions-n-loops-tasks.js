72538819fdee31ae161757183f99f3e005de09ae
fly_6 <36567300+epfly6@users.noreply.github.com>
2024-11-16 02:29:51 +0300

    feat: isTriangle completed

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -93,8 +93,15 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  const sides = [a, b, c];
+  for (let i = 0; i < sides.length; i += 1) {
+    const sumOfOthers = sides.reduce((sum, side, index) => (index !== i ? sum + side : sum), 0);
+    if (sides[i] >= sumOfOthers) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
